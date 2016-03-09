1a11ed98dbc8b0a74b6fb67b0204ece321ce82d3
deksss <deksss@i.ua>
2016-03-09 16:09:09 +0200

    separate rooms list emmit from conret room emmit

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import Path from 'path';
 import makeStore from './store';
 import uuid from 'uuid';
 
+let currentRooms;
+
 export function startServer(store) {
 
   const app = new Express();
@@ -17,53 +19,69 @@ export function startServer(store) {
 
   app.use(Express.static(Path.join(__dirname, 'public')));
 
+  
   store.subscribe(
-  //here must select data only froom neened room
-  //i think its posible with add to store key: lastFiredRoom
-  // when smtn deal witch store, store must change this value
-  // for this i must wrap all func
-  //and sand data only from this room, and use: socket.to(lastFiredRoom)
-    () => io.emit('state', store.getState().toJS())
+    () => {
+      let previousRooms = currentRooms;
+      currentRooms = store.getState().get('rooms');
+      if (previousRooms !== currentRooms) {
+        io.emit('rooms state', currentRooms.toJS());
+      }
+    }
   );
 
   io.on('connection', (socket) => {
-    socket.emit('state', {rooms: store.getState().toJS().rooms});
+    socket.emit('rooms state', store.getState().toJS().rooms);
     
     socket.on('create', (roomId, playerId, name) => {
-      //socket.to(roomId).emit('your room id', roomId);
+      let currentRoom;
       socket.join(roomId);
       store.dispatch({type: 'CREATE_ROOM', roomId: roomId, playerId: playerId, name: name});
 
       console.log('room create: by player ' + playerId);
       console.log(store.getState().toJS());
 
-      socket.to(roomId).emit('state', store.getState().toJS());
+      socket.to(roomId).emit('state', store.getState().get(roomId).toJS());
       socket.to(roomId).emit('your room id', roomId);
       socket.on('action', store.dispatch.bind(store));
       console.log('new room ID: ' + roomId);
+      store.subscribe(
+        () => {
+          let previousRoom = currentRoom;
+          currentRoom = store.getState().get(roomId);
+          if (previousRoom !== currentRoom) {
+             socket.to(roomId).emit('state', currentRoom.toJS())  ;
+          }
+        }
+      );
     });
 
     socket.on('join', (roomId, playerId, name) => {
+      let currentRoom;
       socket.join(roomId);
       store.dispatch({type: 'JOIN_ROOM', roomId: roomId, playerId: playerId, name: name});
 
-      console.log('room '+ roomId +' join: by player ' + playerId);
-      console.log(store.getState().toJS());
-
       socket.join(roomId);
-      socket.to(roomId).emit('state', store.getState().toJS());
+      socket.to(roomId).emit('state', store.getState().get(roomId).toJS());
       socket.to(roomId).emit('your room id', roomId);
       store.dispatch({type: 'NEXT_TURN', roomId: roomId});
       socket.on('action', store.dispatch.bind(store));
       console.log('player connect to room ID: ' + roomId);
+      store.subscribe(
+        () => {
+          let previousRoom = currentRoom;
+          currentRoom = store.getState().get(roomId);
+          if (previousRoom !== currentRoom) {
+             socket.to(roomId).emit('state', currentRoom.toJS())  ;
+          }
+        }
+      );
     });
 
       socket.on('createVsBot', (roomId, playerId, name) => {
       socket.join(roomId);
       store.dispatch({type: 'CREATE_ROOM_VS_BOT', roomId: roomId, playerId: playerId, name: name});
 
-      console.log('room create: by player ' + playerId);
-      console.log(store.getState().toJS());
 
       socket.to(roomId).emit('state', store.getState().toJS());
       socket.to(roomId).emit('your room id', roomId);
